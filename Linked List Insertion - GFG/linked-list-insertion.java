eded7f35750a422a54afe2079fad0ea5da8c8ac1
Prerna Gupta <91413644+prernagupta2625@users.noreply.github.com>
2023-03-02 19:51:42 +0530

    Added solution - LeetHub

---
+++ Linked List Insertion - GFG/linked-list-insertion.java
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+class GFG{
+    static void printList(Node node) 
+    { 
+        while (node != null) 
+        { 
+            System.out.print(node.data + " "); 
+            node = node.next; 
+        } 
+        System.out.println(); 
+    }
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+            int n = sc.nextInt();
+            Node head = null;
+            for(int i=0; i<n; i++)
+            {
+                int value = sc.nextInt();
+                int indicator = sc.nextInt();
+
+                Solution ob = new Solution();
+                if(indicator == 0)
+                    head = ob.insertAtBeginning(head, value);
+                else
+                    head = ob.insertAtEnd(head, value);
+            }
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+
+// } Driver Code Ends
+
+
+/*
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+class Solution
+{
+    //Function to insert a node at the beginning of the linked list.
+    Node insertAtBeginning(Node head, int x)
+    {
+       Node obj = new Node(x);
+       obj.next=head;
+       head=obj;
+       return(head);
+    }
+    
+    //Function to insert a node at the end of the linked list.
+    Node insertAtEnd(Node head, int x)
+    {
+        Node start=head;
+        if(start!=null)
+        {
+        while(start.next!=null)
+        {
+            start=start.next;
+        }
+        }
+        Node obj1=new Node(x);
+        if(start==null)
+        head=obj1;
+        else
+        start.next=obj1;
+        return(head);
+    }
+}
