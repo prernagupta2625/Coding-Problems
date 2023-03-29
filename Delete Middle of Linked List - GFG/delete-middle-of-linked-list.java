90feefdadaa9b0ae2032c0d63774051359017ca3
Prerna Gupta <91413644+prernagupta2625@users.noreply.github.com>
2023-03-29 17:57:13 +0530

    Added solution - LeetHub

---
+++ Delete Middle of Linked List - GFG/delete-middle-of-linked-list.java
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t > 0){
+        	int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            Solution g = new Solution();
+            head = g.deleteMid(head);
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+   
+
+// } Driver Code Ends
+
+
+/*
+class Node {
+    int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+
+class Solution {
+    Node deleteMid(Node head) {
+        // This is method only submission.
+        // You only need to complete the method.
+        int i=1,count=0,mid;
+        Node ptr=head;
+        Node ptr1=head;
+        while(ptr!=null)
+        {
+            ptr=ptr.next;
+            count++;
+        }
+    mid=count/2+1;
+    if(mid==1)
+    {
+        head=null;
+    }
+    else
+    {
+        while(i<mid-1)
+        {
+            ptr1=ptr1.next;
+            i++;
+        }
+    
+        ptr1.next=ptr1.next.next;
+    }
+        return(head);
+    }
+}
