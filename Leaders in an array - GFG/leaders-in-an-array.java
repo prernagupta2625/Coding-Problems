d00f0d66118d920dc7ecd3f91a2bd3d71a231352
Prerna Gupta <91413644+prernagupta2625@users.noreply.github.com>
2023-07-10 14:07:11 +0530

    Added solution - LeetHub

---
+++ Leaders in an array - GFG/leaders-in-an-array.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Array {
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter ot = new PrintWriter(System.out);
+		int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+		
+		while(t-->0){
+		    
+		    //input size of array
+		    int n = Integer.parseInt(br.readLine().trim());
+		    int arr[] = new int[n];
+		    String inputLine[] = br.readLine().trim().split(" ");
+		    
+		    //inserting elements in the array
+		    for(int i=0; i<n; i++){
+		        arr[i] = Integer.parseInt(inputLine[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    
+		    StringBuffer str = new StringBuffer();
+		    ArrayList<Integer> res = new ArrayList<Integer>();
+		  
+		    //calling leaders() function
+		    res = obj.leaders(arr, n);
+		    
+
+		    for(int i=0; i<res.size(); i++){
+		        ot.print(res.get(i)+" ");
+		    }
+		    
+		    ot.println();
+		}
+		ot.close();
+		
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution{
+    //Function to find the leaders in the array.
+    static ArrayList<Integer> leaders(int arr[], int n){
+        // Your code here
+        int max=arr[n-1];
+        ArrayList<Integer> list=new ArrayList<Integer>();
+        for(int i=n-1;i>=0;i--)
+        {
+            if(arr[i]>=max)
+            {
+                list.add(arr[i]);
+                max=arr[i];
+            }
+        }
+        
+        ArrayList<Integer> list2=new ArrayList<Integer>();
+        for(int j=list.size()-1;j>=0;j--)
+        {
+            list2.add(list.get(j));
+        }
+        return list2;
+    }
+}
