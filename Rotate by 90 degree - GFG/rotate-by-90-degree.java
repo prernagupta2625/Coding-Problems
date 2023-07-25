aa2c3720c7e6556c80d567447f9d18f3b054aabc
Prerna Gupta <91413644+prernagupta2625@users.noreply.github.com>
2023-07-26 00:24:48 +0530

    Added solution - LeetHub

--- Rotate by 90 degree - GFG/rotate-by-90-degree.java
+++ Rotate by 90 degree - GFG/rotate-by-90-degree.java
@@ -1,68 +1,69 @@
 //{ Driver Code Starts
 //Initial Template for Java
 
-import java.util.*; 
 import java.io.*;
-import java.lang.*;
+import java.util.*;
 
-class DriverClass
+class GFG
 {
-	public static void main(String[] args) 
-	{ 
-	    Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
+    public static void main(String args[])throws IOException
+    {
+         BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t= Integer.parseInt(read.readLine());
         
-        while (t-- > 0)
+        while(t-- > 0)
         {
-            int n = sc.nextInt();
-            int[][] arr = new int[n][n];
-            
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < n; j++)
-                    arr[i][j] = sc.nextInt();
-            
-            GFG g = new GFG();
-            g.rotate(arr);
-            printMatrix (arr);
+            int n = Integer.parseInt(read.readLine());
+            int matrix[][] = new int[n][n];
+            String st[] = read.readLine().trim().split("\\s+");
+            int k = 0;
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                    matrix[i][j] = Integer.parseInt(st[k++]);
+            }
+            Solution ob = new Solution();
+            ob.rotateby90(matrix, n);
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                    System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
         }
-	} 
-	
-	static void printMatrix(int arr[][]) 
-	{ 
-		for (int i = 0; i < arr.length; i++) { 
-			for (int j = 0; j < arr[0].length; j++) 
-				System.out.print(arr[i][j] + " "); 
-			System.out.println(""); 
-		} 
-	} 
+    }
 }
+
 // } Driver Code Ends
 
 
 //User function Template for Java
 
-class GFG
+class Solution
 {
-    static void rotate(int matrix[][]) 
-    {
-        
-        int n=matrix.length,tmp;
+    //Function to rotate matrix anticlockwise by 90 degrees.
+    static void rotateby90(int matrix[][], int n) 
+    { 
+        // code here
+        int tmp;
         for(int i=0;i<n;i++)
         {
-            for(int j=i+1;j<n;j++)
+            for(int j=i;j<n;j++)
             {
                 tmp=matrix[i][j];
                 matrix[i][j]=matrix[j][i];
                 matrix[j][i]=tmp;
             }
         }
-        for(int k=0;k<n/2;k++)
+        for(int i=0;i<=n/2-1;i++)
         {
-            for(int l=0;l<n;l++)
+            for(int j=0;j<n;j++)
             {
-                tmp=matrix[k][l];
-                matrix[k][l]=matrix[n-1-k][l];
-                matrix[n-1-k][l]=tmp;
+                
+                tmp=matrix[i][j];
+                matrix[i][j]=matrix[n-1-i][j];
+                matrix[n-1-i][j]=tmp;
+                //System.out.print("tmp"+tmp);
             }
         }
     }
