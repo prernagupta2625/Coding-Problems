b9105ca1028d535e78c3a960c66c1cea58c18288
Prerna Gupta <91413644+prernagupta2625@users.noreply.github.com>
2023-08-04 15:23:06 +0530

    Added solution - LeetHub

--- Mirror Tree - GFG/mirror-tree.java
+++ Mirror Tree - GFG/mirror-tree.java
@@ -128,12 +128,13 @@ class Node
 class Solution {
     // Function to convert a binary tree into its mirror tree.
     void mirror(Node node) {
+        // Your code here
         if(node==null)
         return;
         mirror(node.left);
         mirror(node.right);
-        Node tmp=node.right;
-        node.right=node.left;
-        node.left=tmp;
+        Node n=node.left;
+        node.left=node.right;
+        node.right=n;
     }
 }
