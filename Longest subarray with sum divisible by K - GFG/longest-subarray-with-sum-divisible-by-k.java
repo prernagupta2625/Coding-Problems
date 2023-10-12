39277da4e0681af5ee7717634589d29d5c89fe4d
Prerna Gupta <91413644+prernagupta2625@users.noreply.github.com>
2023-10-12 14:54:40 +0530

    Added solution - LeetHub

---
+++ Longest subarray with sum divisible by K - GFG/longest-subarray-with-sum-divisible-by-k.java
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution{
+    int longSubarrWthSumDivByK(int a[], int n, int k)
+    {
+        // Complete the function
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        int sum=0,max=0,rem;
+        for(int i=0;i<n;i++)
+        {
+            int num=a[i];
+            sum=sum+num;
+            if(sum%k==0)
+            max=Math.max(max,i+1);
+            else
+            {
+                rem=sum%k;
+                if(rem<0)
+                rem+=k;
+                if(hm.containsKey(rem))
+                {
+                     max=Math.max(max,i-hm.get(rem));
+                }
+                else
+                {
+                    hm.put(rem,i);
+                }
+            }
+            
+        
+        }
+        
+       return max;
+    }
+ 
+}
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class GFG {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    
+		    String line1 = br.readLine();
+		    String[] element = line1.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    int K = Integer.parseInt(element[1]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		   
+		    int res = obj.longSubarrWthSumDivByK(arr, sizeOfArray, K);
+		    
+		    System.out.println(res);
+		    
+		    
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
